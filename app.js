21dd7ceeee3c5a240e7c0b09015331580bb4d730
Afikur Rahman <this.afikur@gmail.com>
2019-09-04 19:18:23 +0600

    feat: add registration

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
+const dbURI = require("./config/keys").MongoURI;
 const indexRoute = require("./routes");
 const usersRoute = require("./routes/users");
 
 const app = express();
+
+// DB Config
+mongoose
+  .connect(dbURI, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB connected."))
+  .catch(err => console.log(err));
+
 app.use(expressLayouts);
 app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRoute);
 app.use("/users", usersRoute);
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const User = require("./../models/User");
 
 router.get("/login", (req, res) => {
   res.render("login");
@@ -7,6 +9,28 @@ router.get("/login", (req, res) => {
 
 router.get("/register", (req, res) => {
   res.render("register");
+});
+
+router.post("/register", (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  const newUser = new User({
+    name,
+    email,
+    password
+  });
+
+  bcrypt.genSalt(10, (err, salt) =>
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err;
+      newUser.password = hash;
+      newUser
+        .save()
+        .then(user => {
+          res.redirect("/users/login");
+        })
+        .catch(err => console.log(err));
+    })
+  );
 });
 
 module.exports = router;
